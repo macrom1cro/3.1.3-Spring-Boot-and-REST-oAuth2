3f58b806ed5827f71ccaf80fb923c175a15ad3c1
Artem <macromicro37@gmail.com>
2021-08-23 11:23:21 +0300

    220821 final

--- src/main/java/crud/security/SecurityConfig.java
+++ src/main/java/crud/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -35,11 +36,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-//                .antMatchers("/admin/**").access("hasRole('ADMIN')")
-                .antMatchers("/admin/**").hasRole( "ADMIN")
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-                .anyRequest().authenticated()
-                .and().formLogin().successHandler(loginSuccessHandler);
-    }
+
+            http.formLogin()
+                    // указываем страницу с формой логина
+                    .loginPage("/login")
+    //указываем логику обработки при логине
+                .successHandler(loginSuccessHandler)
+    // указываем action с формы логина
+                .loginProcessingUrl("/login")
+    // Указываем параметры логина и пароля с формы логина
+                .usernameParameter("username")
+                .passwordParameter("password")
+    // даем доступ к форме логина всем
+                .permitAll();
+
+        http.logout()
+                // разрешаем делать логаут всем
+                .permitAll()
+    // указываем URL логаута
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            // указываем URL при удачном логауте
+            .logoutSuccessUrl("/login?logout")
+    //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
+                .and().csrf().disable();
+
+        http
+                // делаем страницу регистрации недоступной для авторизированных пользователей
+                .authorizeRequests()
+                //страницы аутентификаци доступна всем
+                .antMatchers("/login").anonymous()
+    // защищенные URL
+                .antMatchers("/admin/**").access("hasAnyRole('ADMIN')")
+                .antMatchers("/user").access("hasAnyRole('USER')")
+    //Все остальные страницы требуют аутентификации
+                .anyRequest().authenticated();
+}
 }
