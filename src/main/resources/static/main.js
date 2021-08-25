3f950ed6590cc037c266a943424bc92f185ed41c
Artem <macromicro37@gmail.com>
2021-08-25 14:49:16 +0300

    250821 день

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,4 +1,4 @@
-// alert('hi');
+
 fetch("http://localhost:8080/admin/users").then(
     res => {
         res.json().then(
@@ -6,19 +6,18 @@ fetch("http://localhost:8080/admin/users").then(
                 console.log(data);
                 if (data.length > 0) {
                     var temp = "";
-                    var generatedHTML = "";
-                    data.forEach((u) => {
+                    data.forEach((user) => {
                         temp += "<tr>"
-                        temp += "<td>" + u.id + "</td>"
-                        temp += "<td>" + u.firstName + "</td>"
-                        temp += "<td>" + u.lastName + "</td>"
-                        temp += "<td>" + u.age + "</td>"
-                        temp += "<td>" + u.email + "</td>"
-                        temp += "<td>" + u.roles.forEach(role => {
-                            generatedHTML += `<span>${role.role.substring(5)}</span>`;
+                        temp += "<td>" + user.id + "</td>"
+                        temp += "<td>" + user.firstName + "</td>"
+                        temp += "<td>" + user.lastName + "</td>"
+                        temp += "<td>" + user.age + "</td>"
+                        temp += "<td>" + user.email + "</td>"
+                        temp += "<td>" + user.roles.forEach(role => {
+                            temp += `<span>${role.role.substring(5)}</span>`;
                         }) + "</td>"
-                        temp += "<td>" + `<a th:href='@{/admin/users/(id=${u.id})}' class='btn btn-primary'>Edit</a>` + "</td>"
-                        temp += "<td><button type='button' class='btn btn-danger' id='open-popup'>Delete</button></td>"
+                        temp += "<td>" + `<a href="/admin/users/${user.id}" class="btn btn-primary" id="edit">Edit</a>` + "</td>"
+                        temp += "<td>" + `<a href="/admin/users/${user.id}" class="btn btn-danger" id="delete">Delete</a>` + "</td>"
                         temp += "</tr>"
                     })
                     document.getElementById("data").innerHTML = temp;
@@ -27,9 +26,32 @@ fetch("http://localhost:8080/admin/users").then(
         )
     })
 
+// fetch("http://localhost:8080/admin/users/1").then(
+//     res => {
+//         res.json().then(
+//             data => {
+//                 console.log(data);
+//                 if (data.length > 0) {
+//                     var temp = "";
+//                     data.forEach((user) => {
+//                         document.getElementById("idEdit").innerHTML = user.id
+//                         document.getElementById("firstNameEdit").innerHTML = user.firstName
+//                         document.getElementById("lastNameEdit").innerHTML = user.lastName
+//                         document.getElementById("ageEdit").innerHTML = user.age
+//                         document.getElementById("emailEdit").innerHTML = user.email
+//                         document.getElementById("passwordEdit").innerHTML = user.password
+//                         document.getElementById("rolesEdit").innerHTML = user.role
+//                     })
+//                     document.querySelector('#editModal').show()
+//                 }
+//             }
+//         )
+//     })
 
-$(document).ready(function () {
-    $('.table .btn').on('click', function (event) {
+// alert('hi');
+window.onload = function(){
+// $(document).ready(function () {
+    $('.btn').on('click', function (event) {
         event.preventDefault();
         var href = $(this).attr('href');
         $.get(href, function (user, status) {
@@ -43,7 +65,23 @@ $(document).ready(function () {
         });
         $('#editModal').modal();
     });
-});
+// });
+}
 
-
+// $(document).ready(function () {
+//     $('#delete').on('click', function (event) {
+//         event.preventDefault();
+//         var href = $(this).attr('href');
+//         $.get(href, function (user, status) {
+//             $('#idEdit').val(user.id);
+//             $('#firstNameEdit').val(user.firstName);
+//             $('#lastNameEdit').val(user.lastName);
+//             $('#ageEdit').val(user.age);
+//             $('#emailEdit').val(user.email);
+//             $('#passwordEdit').val(user.password);
+//             $('#rolesEdit').val(user.role);
+//         });
+//         $('#editModal').modal();
+//     });
+// });
 
